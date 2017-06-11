420a1fb917ccfe3c9c6d382aaef0cf1da8049251
Waldir Bertazzi Junior <waldirbj@gmail.com>
2017-06-11 16:03:21 -0300

    insertion sort

--- helpers/arrays.cpp
+++ helpers/arrays.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 #include "arrays.h"
 
 void print_array(size_t *arr, size_t size) {
@@ -16,6 +18,8 @@ void initialize_array(size_t *arr, size_t size) {
 }
 
 void fill_with_random_data(size_t *arr, size_t size) {
+    srand(time(NULL));
+
     for (size_t i = 0; i < size; i++) {
         arr[i] = (rand()%10)+1;
     };
--- sorts/insertion.cpp
+++ sorts/insertion.cpp
@@ -1,18 +1,46 @@
-#include <iostream>
+    #include <iostream>
 #include <string>
 
 #include "../helpers/arrays.h"
 
-#define MAXSIZE 100
+#define ARRAY_SIZE 10
 
 void print(std::string s) {
     std::cout << s << std::endl;
 }
 
+void insertion_sort(size_t * arr) {
+    int i, j;
+    size_t chave;
+
+    for (i = 1; i < ARRAY_SIZE; i++) {
+        // Primeiro elemento deve
+        // estar no lugar certo.
+		chave = arr[i];
+        // j indica o ultimo indice que já foi
+        // arranjado.
+		j = i - 1;
+
+        // enquanto j nao for -1 e
+        // o elemento anterior e maior
+        // que o elemento chave (a ser arrumado)
+		while ((j >= 0) && (arr[j] < chave)) {
+			arr[j + 1] = arr[j];
+			j--;
+		}
+
+		arr[j + 1] = chave;
+	}
+}
+
 int main()
 {
-    size_t* array = new size_t[MAXSIZE];
+    size_t* array = new size_t[ARRAY_SIZE];
 
-    fill_with_random_data(array, MAXSIZE);
-    print_array(array, MAXSIZE);
+    fill_with_random_data(array, ARRAY_SIZE);
+
+    // Do the job
+    print_array(array, ARRAY_SIZE);
+    insertion_sort(array);
+    print_array(array, ARRAY_SIZE);
 }
